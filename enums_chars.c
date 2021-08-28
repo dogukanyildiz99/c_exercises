39ebbb72c2a58557ef8f2b3f0f41e56404d11526
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2021-08-28 14:22:12 +0300

    Create enums_chars.c

---
+++ enums_chars.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    enum city {Istabul, Ankara, Izmir, Bursa, Antalya, Adana};
+    enum city myCity;
+    myCity = Adana;
+
+    char myCharacter = 'D';
+    char myEscapeChar = '\n';
+    int myAge = 22;
+
+    printf("First letter of my name is %c %cMy age is %d", myCharacter, myEscapeChar, myAge);
+
+
+    return 0;
+}
