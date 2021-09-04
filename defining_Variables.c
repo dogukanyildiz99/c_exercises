f29d27eb0be349975525f98f7fad43640f73d49b
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2021-09-04 19:01:17 +0300

    Create defining_Variables.c

---
+++ defining_Variables.c
@@ -0,0 +1,9 @@
+#include <stdlib.c>
+#include <stdio.h>
+
+int main (){
+  int a = 0, b = 1, c = 2, d = 3, e = 4;
+  a = b - c + d * e;
+  printf("%d", a); /* will print 1-2+3*4 = 11 */
+  return 0;
+}
