a5afd1f8cb1f45a5aa6e2ab3859d83ffe0eb2af6
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2022-01-25 16:38:33 +0300

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -8,26 +8,29 @@ int main()
     // declaring pointers
     int *p;
     int *d;
+    // pointer that points to an another pointer
+    int **q;
     // some integers
     int a = 10;
     int b = 20;
     // pointers refers to these addresses
     p = &a;
     d = &b;
+    q = &p;
     // the values that stored in the addresses that pointers referred
-    printf("%d %d\n", *p, *d);
+    printf("%d %d %d\n", *p, *d, **q);
     // the addresses that pointers stored
-    printf("%d %d\n", p, d);
+    printf("%d %d %d\n", p, d, q);
     // the addresses of the pointers itselves
-    printf("%d %d", &p, &d);
+    printf("%d %d %d", &p, &d, &q);
 
     return 0;
 }
 
 /*
-------Output-------
-    10 20
-    6422028 6422024
-    6422040 6422032
--------------------
+------------Output-------------
+    10 20 10
+    6422020 6422016 6422040
+    6422040 6422032 6422024
+-------------------------------
 */
